97f7b0a5093d42e6f4eda3a20899aa711277887d
seannnnnn1017 <op23756778@gmail.com>
2023-06-16 13:43:15 +0800

    123

--- test.php
+++ test.php
@@ -1,2 +1,12 @@
-<?php
-phpinfo();
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="test1.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
